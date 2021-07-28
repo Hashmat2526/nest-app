21934adc379cb710c7b39f6a966cc612f0a16233
hashmat2526 <hashmatajalali@gmail.com>
2021-07-28 17:53:22 +0500

    completed before auth

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //set global api prefix
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000, () =>
     console.log('server is up and running on port 3000'),
   );
